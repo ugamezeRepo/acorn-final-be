fb730ad7ac77098ad2a0d28707696659063fcfcd
sonsanghee <sonsanghee3@gmail.com>
2024-02-27 16:59:33 +0900

    주석 제거

--- src/main/java/com/acorn/finals/controller/TokenController.java
+++ src/main/java/com/acorn/finals/controller/TokenController.java
@@ -18,25 +18,5 @@ public class TokenController {
     public AccessTokenDto issueAccessToken(@RequestHeader("x-refresh-token") String refreshToken) {
         return tokenService.issueAccessToken(refreshToken);
     }
-
-//    @GetMapping("/jwt")
-//    public String login(String userId) {
-//        return tokenService.generateToken("hohoho");
-//
-//    }
-//
-//    @GetMapping("/jwt/validation")
-//    public boolean jwtCheck(String token) {
-//        return tokenService.validateToken(token);
-//    }
-//
-//    @GetMapping("jwt/test")
-//    public String jwtTest() {
-//        return SecurityContextHolder.getContext().getAuthentication().getName();
-//    }
-//
-//    @GetMapping("jwt/refresh")
-//    public String refresh() {
-//        return tokenService.createRefreshToken("son");
-//    }
+    
 }
