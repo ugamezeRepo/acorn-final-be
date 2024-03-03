7121c5d5ef9e5a80587922ff6ef5ee38e9d1bce3
cloudchamb3r <jizon0123@protonmail.com>
2024-03-03 17:29:02 +0900

    change cors filter => jakarta.servlet.filter

--- src/main/java/com/acorn/finals/filter/CorsFilter.java
+++ src/main/java/com/acorn/finals/filter/CorsFilter.java
@@ -1,30 +1,44 @@
 package com.acorn.finals.filter;
 
 import com.acorn.finals.config.properties.FrontendPropertiesConfig;
+import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @RequiredArgsConstructor
-public class CorsFilter extends OncePerRequestFilter {
+public class CorsFilter implements Filter {
     private final FrontendPropertiesConfig frontendPropertiesConfig;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", frontendPropertiesConfig.getUrl());
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
-        filterChain.doFilter(request, response);
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        if (response instanceof HttpServletResponse resp) {
+            resp.setHeader("Access-Control-Allow-Origin", frontendPropertiesConfig.getUrl());
+            resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            resp.setHeader("Access-Control-Allow-Credentials", "true");
+            resp.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
     }
 }
