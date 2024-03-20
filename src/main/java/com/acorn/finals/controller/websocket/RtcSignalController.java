51c24db45a2e5d0470d44ee91f87ce84b8f7b9eb
cloudchamb3r <jizon0123@protonmail.com>
2024-03-20 22:42:25 +0900

    wip

--- src/main/java/com/acorn/finals/controller/websocket/RtcSignalController.java
+++ src/main/java/com/acorn/finals/controller/websocket/RtcSignalController.java
@@ -2,26 +2,17 @@ package com.acorn.finals.controller.websocket;
 
 import com.acorn.finals.annotation.WebSocketController;
 import com.acorn.finals.annotation.WebSocketMapping;
-import com.acorn.finals.annotation.WebSocketOnClose;
-import com.acorn.finals.annotation.WebSocketOnConnect;
-import com.acorn.finals.model.WebSocketSessionInfo;
 import com.acorn.finals.model.dto.websocket.RtcSignalDto;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 
 @AllArgsConstructor
@@ -46,16 +37,17 @@ public class RtcSignalController {
     }
 
     @WebSocketMapping("/channel/{channelId}/topic/{topicId}")
-    public RtcSignalDto handleTopicSignal(@RequestBody RtcSignalDto dto, @PathVariable int channelId, @PathVariable int topicId, WebSocketSession ws) {
+    public RtcSignalDto handleTopicSignal(@RequestBody RtcSignalDto dto, @PathVariable int channelId,
+                                          @PathVariable int topicId, WebSocketSession ws) {
         var roomInfo = new RoomInfo(channelId, topicId);
         participantUUIDs.computeIfAbsent(roomInfo, (k) -> new HashMap<>()).put(ws, dto.getUuid());
         return dto;
     }
 
-    @WebSocketOnClose("/channel/{channelId}/topic/{topicId}")
-    public RtcSignalDto handleTopicClose(@PathVariable int channelId, @PathVariable int topicId, WebSocketSession ws) {
-        var roomInfo = new RoomInfo(channelId, topicId);
-        var uuid = participantUUIDs.computeIfAbsent(roomInfo, (k) -> new HashMap<>()).remove(ws);
-        return new RtcSignalDto(null, Map.of("type", "remove"), uuid);
-    }
+//    @WebSocketOnClose("/channel/{channelId}/topic/{topicId}")
+//    public RtcSignalDto handleTopicClose(@PathVariable int channelId, @PathVariable int topicId, WebSocketSession ws) {
+//        var roomInfo = new RoomInfo(channelId, topicId);
+//        var uuid = participantUUIDs.computeIfAbsent(roomInfo, (k) -> new HashMap<>()).remove(ws);
+//        return new RtcSignalDto(null, Map.of("type", "remove"), uuid);
+//    }
 }
